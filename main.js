b1c9fdc1c0bf68594c22679829b9b884a0099274
Soni <soni801_@outlook.com>
2022-01-17 19:38:26 +0100

    fix: fix httpss lmao

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 // Cheap-ass https redirect
 const url = window.location.href;
-if (url.startsWith("http")) window.location = url.replace("http", "https");
+if (url.startsWith("http:")) window.location = url.replace("http", "https");
